fb7a44ad1f29698b041098a7973783992e4d4eb5
Mo <93756682+mightySIZE@users.noreply.github.com>
2023-03-20 13:07:42 -0700

    Add files via upload

---
+++ mhasan5.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int[], int);
+void print(int[], int, int);
+
+int main() {
+    int size;
+    cin >> size;
+    int arr[size];
+    for (int i=0; i<size; i++)
+        cin >> arr[i];
+
+    insertionSort(arr, size);
+    return 0;
+}
+
+void insertionSort(int arr[], int size) {
+    for (int i=1; i<(int)size ; i++) {
+        int sortedKey = arr[i];
+        int j = i-1;
+        while ((j>=0) && (arr[j]>sortedKey)) {
+            arr[j+1] = arr[j];
+            j = j - 1;
+        }
+        arr[j+1] = sortedKey;
+        print(arr, i, size);
+    }
+    return;
+}
+
+void print(int arr[], int i, int size) {
+    int end = i+1;
+    if (end > size) {end = size;}
+    for (int x=0; x<end; x++) {
+        if (end > size)
+            end = size;
+        cout << arr[x] << ";";
+    }
+    cout << endl;
+}
